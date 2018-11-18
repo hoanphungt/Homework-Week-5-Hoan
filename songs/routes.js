a134336c60b4fdd13dc92d3fa88e1fac47503d31
Hoan Phung <hoanphung.ptso1@gmail.com>
2018-11-18 12:01:28 +0100

    check if songs already existed

--- songs/routes.js
+++ songs/routes.js
@@ -13,31 +13,32 @@ router.post('/playlists/:id/songs', (req, res, next) => {
                     message: `This playlist does not exist`
                 })
             } else {
-                if (
-                    Song
-                        .findOne({ where: { title: req.body.title } })
-                ) {
-                    res.status(400).send({
-                        message: 'This song already existed'
-                    })
-                } else {
-                    Song
-                        .create({
-                            title: req.body.title,
-                            artist: req.body.artist,
-                            album: req.body.album,
-                            playlistId: playlist.id
-                        })
-                        .then(song => {
-                            if (!song) {
-                                return res.status(404).send({
-                                    message: `This song does not exist`
+                Song
+                    .findOne({ where: { title: req.body.title } })
+                    .then(song => {
+                        if (song) {
+                            return res.status(400).send({
+                                message: `This song already existed`
+                            })
+                        } else {
+                            Song
+                                .create({
+                                    title: req.body.title,
+                                    artist: req.body.artist,
+                                    album: req.body.album,
+                                    playlistId: playlist.id
                                 })
-                            }
-                            return res.status(201).send(song)
-                        })
-                        .catch(error => next(error))
-                }
+                                .then(song => {
+                                    if (!song) {
+                                        return res.status(404).send({
+                                            message: `This song does not exist`
+                                        })
+                                    }
+                                    return res.status(201).send(song)
+                                })
+                                .catch(error => next(error))
+                        }
+                    })
             }
         })
         .catch(error => next(error))
